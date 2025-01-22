dd70ed3d982afabacbfb9871f5c9f9bd3b6de13b
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-21 19:35:25 -0600

    Add outline of program

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,25 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
+    // check if we have the TOR node
+
+    // if not buy TOR node when we have the money
+
+    // run script to buy from TOR node (while loop)
+
+    // run search script
+    // return internet
+
+    // run getRoot script (while loop)
+    
+    // copypayload
+
+    // attack (while loop w/ search)
+
+    // buy servers (while loop)
+
+    // buy hacknet (while loop)
+
+    // progress through milestones
     
 }
