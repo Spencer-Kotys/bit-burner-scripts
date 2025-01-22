023200daaff4c8b1d540b2ccc9514afbb349566b
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-22 06:37:18 -0600

    Make TOR script outline

---
+++ src/buystuff.ts
@@ -0,0 +1,9 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+    // check if we have the TOR node
+    // if not buy TOR node when we have the money
+
+    // run script to buy from TOR node (while loop)
+
+}
--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,8 @@
 import { NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
-    // check if we have the TOR node
-
-    // if not buy TOR node when we have the money
-
-    // run script to buy from TOR node (while loop)
+    // run TOR script
+    ns.exec("buystuff.js", "home");
 
     // run search script
     // return internet
@@ -21,5 +18,5 @@ export async function main(ns: NS): Promise<void> {
     // buy hacknet (while loop)
 
     // progress through milestones
-    
+
 }
