cb168519599db104784e4db47d4a48062b310148
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-22 07:15:08 -0600

    Trying  to execute terminal commands

--- src/buystuff.ts
+++ src/buystuff.ts
@@ -1,8 +1,27 @@
 import { NS } from "@ns";
+import { spawn } from "child_process";
 
 export async function main(ns: NS): Promise<void> {
-    // check if we have the TOR node
-    // if not buy TOR node when we have the money
+    // trying to execute terminal commands
+    const ls = spawn("ls", ["-lh", "/"]);
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+      
+      ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      });
+      
+      ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+      });
+    // while we don't have the TOR node
+    /*
+    while (!(ns.hasTorRouter())) {
+        // check if we have the money to buy it
+        
+    }
+    */
 
     // run script to buy from TOR node (while loop)
 
