8fdd19b5299e4777e5da58bf931b8227417b4474
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-22 07:40:30 -0600

    trying html inject

--- src/buystuff.ts
+++ src/buystuff.ts
@@ -1,20 +1,17 @@
 import { NS } from "@ns";
-import { spawn } from "child_process";
 
 export async function main(ns: NS): Promise<void> {
-    // trying to execute terminal commands
-    const ls = spawn("ls", ["-lh", "/"]);
-    ls.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-      });
-      
-      ls.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-      
-      ls.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-      });
+    // trying to execute terminal commands with HTML inject
+    // get reference to terminal text field
+    let terminalInput = document.getElementById("terminal-input");
+    // set value to run
+    terminalInput.value = "home;buy -l;";
+    // get reference to event handler
+    let handler = Object.keys(terminalInput)[1];
+    // set internal values
+    terminalInput[handler].onChange({target:terminalInput});
+    // simulate enter
+    terminalInput[handler].onKeyDown({key: "Enter", preventDefault:()=>null});
     // while we don't have the TOR node
     /*
     while (!(ns.hasTorRouter())) {
