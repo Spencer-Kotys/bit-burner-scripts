ade662651be5138da6c976a204ea2ff4f522643d
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-22 12:54:09 -0600

    Creating milestones.ts

---
+++ src/milestones.ts
@@ -0,0 +1,28 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+    // log the start of the script
+    ns.print("Starting milestones script");
+    // gain root access on CSEC (we will wait for our hack script to do this)
+    while (!ns.hasRootAccess("CSEC")) {
+        ns.sleep(10000);
+    }
+    // log that we have root access on CSEC
+    ns.print("Root access on CSEC acquired");
+    // install backdoor on CSEC
+    // wait until we have the hacking level required to install the backdoor
+    while (ns.getServerRequiredHackingLevel("CSEC") > ns.getHackingLevel()) {
+        ns.sleep(10000);
+    }
+    // join CyberSec
+
+    // install all augemtations from CyberSec
+
+    // join NiteSec
+
+    // install all augmentations from NiteSec
+
+    // join BlackHand
+
+    // install all augmentations from BlackHand
+}
