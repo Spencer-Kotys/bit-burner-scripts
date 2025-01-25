791e1fa10095b69a39345d67869045dac736a292
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-25 07:57:01 -0600

    Add get root and copy scripts to start

--- src/start.ts
+++ src/start.ts
@@ -3,12 +3,16 @@ import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
     // run TOR script (need better tech for this, must be manual for now)
     //ns.exec("buystuff.js", "home");
-    // run search, root, and copy script
+    // run search
     ns.run("getServers.js");
     // wait for port 1 to be written to
     await ns.nextPortWrite(1);
     // peek port 1
     let internet: string[] = ns.peek(1);
+    // run root script
+    ns.run("getRoot.js", 1, ...internet);
+    // run copy script
+    ns.run("copyScripts.js", 1, ...internet);
     // attack (while loop w/ search)
     ns.run("moneyMaker.js");
     // buy servers (while loop)
