21bc993419b19f92e57dbe8e90b2e029d4ea572d
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-25 09:20:06 -0600

    Add weaken logic

--- src/moneyMaker.ts
+++ src/moneyMaker.ts
@@ -2,8 +2,8 @@ import { NS } from "@ns";
 
 function getTarget(ns: NS, internet: string[]): string {
     // create base variables
-    let targetName = "";
-    let highestPercentage = 0;
+    let targetName: string = "";
+    let highestPercentage: number = 0;
     // loop through internet array
     for (let i = 0; i < internet.length; ++i) {
         const server = internet[i];
@@ -12,7 +12,7 @@ function getTarget(ns: NS, internet: string[]): string {
             // check if server is hackable
             if (ns.getServerRequiredHackingLevel(server) <= ns.getHackingLevel()) {
                 // get percentage of money that is hacked with one thread
-                const percentage = ns.hackAnalyze(server);
+                let percentage: number = ns.hackAnalyze(server);
                 // check if percentage of money hacked is the highest percentage yet
                 if (percentage > highestPercentage) {
                     // set highest percentage and target name to new server
@@ -37,13 +37,13 @@ function checkInternet(ns: NS, internet: string[]): string[] {
 }
 
 // calculate number of threads to run on server
-function numOfThreads(ns: NS, server: string, scriptRam: number) {
+function getNumOfThreads(ns: NS, server: string, scriptRam: number) {
     // get amount of RAM on server
-    const serverRam = ns.getServerMaxRam(server);
+    const serverRam: number = ns.getServerMaxRam(server);
     // get current RAM used
-    const usedRam = ns.getServerUsedRam(server);
+    const usedRam: number = ns.getServerUsedRam(server);
     // calculate number of threads to run based on Ram, round down to nearest whole number
-    const threads = Math.floor((serverRam - usedRam) / scriptRam);
+    const threads: number = Math.floor((serverRam - usedRam) / scriptRam);
     // return number of threads
     return threads;
   }
@@ -51,10 +51,30 @@ function numOfThreads(ns: NS, server: string, scriptRam: number) {
 export async function main(ns: NS): Promise<void> {
     // declare internet as array of strings
     let internet: string[] = ns.args as string[];
+    // get script Ram requirements
+    const weakenRam: number = ns.getScriptRam("w.js", "home");
+    const growRam: number = ns.getScriptRam("g.js", "home");
+    const hackRam: number = ns.getScriptRam("h.js", "home");
     // run while loop
     while (true) {
         // get best target based on highest percentage of money hacked per thread
         let target: string = getTarget(ns, internet);
+        // check if security level is above min security level
+        if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+            // if security is greater than current level
+      if (ns.getServerSecurityLevel(target) > ns.getServerMinSecurityLevel(target)) {
+        // weaken it
+        for (let i = 0; i< internet.length; ++i) {
+          let server: string = internet[i];
+          // get number of threads to run
+          let numOfThreads: number = getNumOfThreads(ns, server, weakenRam);
+          // execute payload on server with number of threads
+          if (numOfThreads > 0) {
+            ns.exec("w.js",server,numOfThreads,target);
+          }
+        }
+      }
+        }
         // check if there are new servers
         internet = checkInternet(ns, internet);
     }
