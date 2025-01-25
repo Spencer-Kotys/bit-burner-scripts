133e20a8eefaa545459bee613577e6d4a401318d
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-25 08:12:23 -0600

    Create money maker script

---
+++ src/moneyMaker.ts
@@ -0,0 +1,7 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+    // declare internet as array of strings
+    let internet: string[] = ns.args as string[];
+    return;
+ }
