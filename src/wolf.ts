60f54f77b6b653a40c174d13f0506668a6711f09
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-26 11:02:39 -0600

    Add buy share functionality

--- src/wolf.ts
+++ src/wolf.ts
@@ -3,20 +3,39 @@ import { NS } from '@ns'
 export async function main(ns: NS): Promise<void> {
     // get array of all stock symbols
     const stockSymbols: string[] = ns.stock.getSymbols();
-    // get current funds
-    const initialFunds: number = ns.getServerMoneyAvailable('home');
-    const maxStocks = 1000;
     // while loop to check stock market
     while (true) {
+        // get current funds minus 1 million
+        let currentFunds: number = (ns.getServerMoneyAvailable('home')-1000000);
+        // declare stock to buy
+        let stockToBuy: string = "";
+        // declare best forecast
+        let bestForecast: number = 0;
         // loop through stock symbols
         for (const symbol of stockSymbols) {
-            const price = ns.stock.getAskPrice(symbol);
-            const sharesToBuy = Math.min(Math.floor(initialFunds / price), maxStocks);
-            
-            if (sharesToBuy > 0) {
-                ns.stock.buyStock(symbol, sharesToBuy);
-                ns.tprint(`Bought ${sharesToBuy} shares of ${symbol} at ${price}`);
-            }
+            // get stock forcast
+            const forecast = ns.stock.getForecast(symbol);
+            // if forecast is the best we have seen
+            if (forecast > bestForecast) {
+                // set stock to buy
+                stockToBuy = symbol;
+                // set best forecast
+                bestForecast = forecast;
+            }  
+        }
+        // get price of stock to buy
+        let price: number = ns.stock.getAskPrice(stockToBuy);
+        // get the max shares of a stock
+        let maxShares: number = ns.stock.getMaxShares(stockToBuy);
+        // get the current shares of the stock we have
+        let currentShares: number = ns.stock.getPosition(stockToBuy)[0];
+        // get the number of shares we can buy
+        let sharesToBuy: number = Math.min(Math.floor(currentFunds / price), maxShares - currentShares);
+        // check if we can buy shares
+        if (sharesToBuy > 0) {
+            // buy shares
+            ns.stock.buyStock(stockToBuy, sharesToBuy);
+            ns.tprint(`Bought ${sharesToBuy} shares of ${stockToBuy} at ${price}`);
         }
         // wait for stock market to update
         await ns.stock.nextUpdate()
