f91f77226b338efc3a0957cd4a27b76fe8a3f435
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-26 11:10:42 -0600

    Add selling of stocks when price goes down

--- src/wolf.ts
+++ src/wolf.ts
@@ -5,6 +5,29 @@ export async function main(ns: NS): Promise<void> {
     const stockSymbols: string[] = ns.stock.getSymbols();
     // while loop to check stock market
     while (true) {
+        // get stock portfolio
+        for (const symbol of stockSymbols) {
+            // get current shares
+            const shares: number = ns.stock.getPosition(symbol)[0];
+            // if we have shares
+            if (shares > 0) {
+                // get forecast
+                const forecast: number = ns.stock.getForecast(symbol);
+                // if forecast is less than 0
+                if (forecast < 0) {
+                    // sell shares
+                    ns.stock.sellStock(symbol, shares);
+                    // get gain
+                    const gain: number = ns.stock.getSaleGain(symbol, shares, "long");
+                    ns.tprint(`Sold ${shares} shares of ${symbol} for a gain of ${gain}`);
+                }
+                // get price of stock
+                const price: number = ns.stock.getBidPrice(symbol);
+                // sell shares
+                ns.stock.sellStock(symbol, shares);
+                ns.tprint(`Sold ${shares} shares of ${symbol} at ${price}`);
+            }
+        }
         // get current funds minus 1 million
         let currentFunds: number = (ns.getServerMoneyAvailable('home')-1000000);
         // declare stock to buy
