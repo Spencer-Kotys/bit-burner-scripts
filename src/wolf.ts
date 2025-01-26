28d2e2cd434cecc60bc5dd0fac933191e2d6b17c
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2025-01-26 10:42:52 -0600

    Add while loop

--- src/wolf.ts
+++ src/wolf.ts
@@ -1,17 +1,24 @@
 import { NS } from '@ns'
 
 export async function main(ns: NS): Promise<void> {
-    const stockSymbols = ns.stock.getSymbols();
-    const initialFunds = ns.getServerMoneyAvailable('home');
+    // get array of all stock symbols
+    const stockSymbols: string[] = ns.stock.getSymbols();
+    // get current funds
+    const initialFunds: number = ns.getServerMoneyAvailable('home');
     const maxStocks = 1000;
-
-    for (const symbol of stockSymbols) {
-        const price = ns.stock.getAskPrice(symbol);
-        const sharesToBuy = Math.min(Math.floor(initialFunds / price), maxStocks);
-        
-        if (sharesToBuy > 0) {
-            ns.stock.buy(symbol, sharesToBuy);
-            ns.tprint(`Bought ${sharesToBuy} shares of ${symbol} at ${price}`);
+    // while loop to check stock market
+    while (true) {
+        // loop through stock symbols
+        for (const symbol of stockSymbols) {
+            const price = ns.stock.getAskPrice(symbol);
+            const sharesToBuy = Math.min(Math.floor(initialFunds / price), maxStocks);
+            
+            if (sharesToBuy > 0) {
+                ns.stock.buyStock(symbol, sharesToBuy);
+                ns.tprint(`Bought ${sharesToBuy} shares of ${symbol} at ${price}`);
+            }
         }
+        // wait for stock market to update
+        await ns.stock.nextUpdate()
     }
 }
